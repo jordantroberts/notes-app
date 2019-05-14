d2d264a5779ddd2f8b5810511483536e8674125e
Jordan Roberts <jordanroberts@talk21.com>
2019-05-14 12:04:33 +0100

    Refactored to use assert

--- note-test.js
+++ note-test.js
@@ -1,10 +1,16 @@
-(function(exports){
-  function testNote(){
-    var note = new Note("hello");
+// (function(exports){
+//   function testNote(){
+//     var note = new Note("hello");
+//
+//     if (note.text !== "hello") {
+//     throw new Error("No note")
+//     };
+//   };
+//   testNote();
+// })(this)
 
-    if (note.text !== "hello") {
-    throw new Error("No note")
-    };
-  };
-  testNote();
-})(this)
+function testNote() {
+  var note = new Note("hello");
+  assert.isTrue(note.text === "hello");
+};
+testNote()
