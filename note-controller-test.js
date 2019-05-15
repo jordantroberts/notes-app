426bd93bd027152025e89ab0396dcbc46e37a615
Jordan Roberts <jordanroberts@talk21.com>
2019-05-15 20:37:48 +0100

    Test to check that note controller can be instantiated

---
+++ note-controller-test.js
@@ -0,0 +1,18 @@
+// Tests to Write
+
+// 1. A note controller can be instantiated.
+
+// 2. The innerHTML property of the app element contains HTML somewhat similar to: <ul><li><div>Favourite food: pesto</div></li></ul>.
+// (You'll need to mock some stuff to achieve this.)
+
+(function(exports){
+  function testNoteControllerCanBeInstantiated(){
+    var notelist = new Notelist()
+    var controller = new Controller(notelist)
+
+    if (!controller instanceof NoteController){
+      throw new Error("Controller not instantiated properly")
+    }
+  }
+  testNoteControllerCanBeInstantiated()
+})(this); 
