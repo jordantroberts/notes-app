15c6c435480f55158991b838e40ea822402552a1
Jordan Roberts <jordanroberts@talk21.com>
2019-05-15 17:58:56 +0100

    Created NoteController function to display notes in index.html page
    New note-controller.js file containing a NoteController function which takes a note list model as a parameter, adds a note, and creates a note list view which passes in the note list model. The note controller has a method that gets HTML from the note list view and inserts it into the app element.
    
    This was done by spiking code in order to know direction and next step is to TDD.

--- note-controller.js
+++ note-controller.js
@@ -1,5 +1,14 @@
 function changeGreeting(greeting) {
   var element = document.getElementById("app");
   greeting = "howdy"
-  element.innerHTML = "greeting"
+  element.innerHTML = greeting
 }
+
+function NoteController(notelist, noteview) {
+    this.notelist = notelist;
+    this.noteview = noteview
+};
+
+NoteController.prototype.updateDOM = function(){
+  document.getElementById('app').innerHTML = this.noteview.display()
+}; // fix this
