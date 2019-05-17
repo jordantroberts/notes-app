c2f8ac6a4e81551a77b642c661efb5b4aae12f04
Jordan Roberts <jordanroberts@talk21.com>
2019-05-17 09:44:44 +0100

    Amended note-list-view.js so only first 20 chars of notes show
    Spiked to  update note list view code to only show the first 20 characters of each note. Will now write tests.

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -3,11 +3,13 @@
     this.notelist = notelist;
   };
 
-NoteView.prototype.display = function() {
- var array = this.notelist.list.map(note => note.text).join("</div></li><li><div>")
- var html = "<ul><li><div>" + array + "</div></li></ul>"
- return html
-};
+
+  NoteView.prototype.display = function() {
+   var array = this.notelist.list.map(note => note.text)
+   var substring = array.map(note => note.substring(0,20)).join("</div></li><li><div>")
+   var html = "<ul><li><div>" + substring + "</div></li></ul>"
+   return html
+  };
 
 exports.NoteView = NoteView;
 })(this);
