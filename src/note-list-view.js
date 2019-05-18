7a017de9408d880b3c8db0847d00741c43aee3e3
Jordan Roberts <jordanroberts@talk21.com>
2019-05-18 20:18:18 +0100

    Each note linked to a page that displays full note

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -6,7 +6,7 @@
   var html = ""
   NoteView.prototype.display = function() {
     this.notelist.list.map(function(note) {
-      html += `<li><div><a href="${note.getID()}">${note.text.substring(0,20)}</a></div></li>`
+      html += `<li><div><a href="${window.location.href}#notes/${note.getID()}">${note.text.substring(0,20)}</a></div></li>`
     });
     return `<ul>${html}</ul>`
   };
